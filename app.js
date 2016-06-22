a685f7c92e95ed62fde8e2711de7f28a187b708e
kissa1001 <kissa101295@gmail.com>
2016-06-22 17:01:02 -0400

    added mongoose conn

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/posts';
-mongoose.connect('mongodb://localhost/blog');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/blog');
 require('./models/Posts');
 require('./models/Comments');
 
